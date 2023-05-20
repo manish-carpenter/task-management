4b131729f2116452868e7cdb8e5727ae12c422f1
manishc <manishcarpenter15@gmail.com>
2023-05-20 18:56:10 +0530

    few fixes in login and authorization section7 done

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,21 +4,33 @@ import { Module } from '@nestjs/common';
 import { TasksModule } from './tasks/tasks.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { configValidationSchema } from './config.schema';
 @Module({
   imports: [
     TasksModule,
-    TypeOrmModule.forRoot({
-      type: 'mssql',
-      host: 'localhost',
-      port: 1433,
-      username: 'sa',
-      password: 'Admin@123',
-      database: 'task-management',
-      //entities: [__dirname + '/../**/*.entity.js'],
-      autoLoadEntities: true,
-      synchronize: true,
-      extra: {
-        trustServerCertificate: true,
+    ConfigModule.forRoot({
+      envFilePath: [`.env.stage.${process.env.STAGE}`],
+      validationSchema: configValidationSchema,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => {
+        return {
+          type: 'mssql',
+          host: configService.get<string>('DB_HOST'),
+          port: +configService.get<number>('DB_PORT'),
+          username: configService.get<string>('DB_USERNAME'),
+          password: configService.get<string>('DB_PASSWORD'),
+          database: configService.get<string>('DB_DATABSE'),
+          //entities: [__dirname + '/../**/*.entity.js'],
+          autoLoadEntities: true,
+          synchronize: true,
+          extra: {
+            trustServerCertificate: true,
+          },
+        };
       },
     }),
     AuthModule,
--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,16 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { TransformInterceptor } from './transform.interceptor';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  const logger = new Logger();
   app.useGlobalPipes(new ValidationPipe());
   app.useGlobalInterceptors(new TransformInterceptor());
-  await app.listen(3000);
+  const port = 3000;
+  await app.listen(port);
+  logger.log(`Application listing on port: ${port}`);
 }
 bootstrap();
